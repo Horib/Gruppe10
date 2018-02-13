a38fe0c9dc3dfbc01d73bc5b362f8c1767a2b2e3
Borgar Bordoy <bobor16@student.sdu.dk>
2018-02-13 12:42:43 +0100

    yeah

--- src/Test12345.java
+++ src/Test12345.java
@@ -1,6 +1,6 @@
 public class Test12345 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Borgar¨s world");
         System.out.println("Oscar er sej");
 }
     
